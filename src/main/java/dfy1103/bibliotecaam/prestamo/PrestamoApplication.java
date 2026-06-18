cf27351117c4dfa8743f5417c9298cf361b49f03
MAGAQ <panamasado41@gmail.com>
2026-06-17 22:48:41 -0400

    Conectado a server config y api gateway, falta la seguridad.

--- src/main/java/dfy1103/bibliotecaam/prestamo/PrestamoApplication.java
+++ src/main/java/dfy1103/bibliotecaam/prestamo/PrestamoApplication.java
@@ -2,8 +2,10 @@ package dfy1103.bibliotecaam.prestamo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 /*
 --------------------------------------- AVISO DE UTILIDAD -------------------------------
 * * * * * NO ACTIVES ESTE MICRO SERVICIO HASTA QUE HAYAS ACTIVADO PRIMERO EL DE USUARIO
